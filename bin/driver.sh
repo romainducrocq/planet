19c7410642f9ecfc06efe6445594b4a421265384
romainducrocq <romain.g.ducrocq@gmail.com>
2025-09-28 19:12:25 +0300

    add driver

---
+++ bin/driver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PACKAGE_DIR="$(dirname $(readlink -f ${0}))"
+BACKEND_PATH="$(readlink -f ${PACKAGE_DIR}/$(cat ${PACKAGE_DIR}/backend_path.txt))"
+export EXTERN_EXT_IN="pl.src"
+
+cd ${BACKEND_PATH}/bin/
+./driver.sh ${@}
+if [ ${?} -ne 0 ]; then exit 1; fi
--- bin/install.sh
+++ bin/install.sh
@@ -2,6 +2,5 @@
 
 BACKEND_PATH="$(readlink -f $(cat backend_path.txt))"
 
-cd ${BACKEND_PATH}/bin/
-./install.sh
+${BACKEND_PATH}/bin/install.sh
 if [ ${?} -ne 0 ]; then exit 1; fi
