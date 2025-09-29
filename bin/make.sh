c5136503465f99c60121a5a375cb977c2ea85277
romainducrocq <romain.g.ducrocq@gmail.com>
2025-09-29 16:47:46 +0300

    rm default frontend

--- bin/make.sh
+++ bin/make.sh
@@ -5,9 +5,11 @@ export EXTERN_FRONTEND_PARSER_DIR="$(readlink -f ../frontend/parser)"
 # export EXTERN_FRONTEND_INTERM_DIR="$(readlink -f ../frontend/intermediate)"
 if [ -d "${BACKEND_PATH}/src/frontend/parser/" ]; then
     mv ${BACKEND_PATH}/src/frontend/parser/ ${BACKEND_PATH}/src/frontend/parser.bak/
+    if [ ${?} -ne 0 ]; then exit 1; fi
 fi
 # if [ -d "${BACKEND_PATH}/src/frontend/intermediate/" ]; then
 #     mv ${BACKEND_PATH}/src/frontend/intermediate/ ${BACKEND_PATH}/src/frontend/intermediate.bak/
+#     if [ ${?} -ne 0 ]; then exit 1; fi
 # fi
 
 cd ${BACKEND_PATH}/bin/
