a4ab3d4acfcbf7a0dbe168b0ff1318d6db4926ec
romainducrocq <romain.g.ducrocq@gmail.com>
2025-09-29 14:44:09 +0300

    set filename extension

--- bin/driver.sh
+++ bin/driver.sh
@@ -2,8 +2,7 @@
 
 PACKAGE_DIR="$(dirname $(readlink -f ${0}))"
 BACKEND_PATH="$(readlink -f ${PACKAGE_DIR}/$(cat ${PACKAGE_DIR}/backend_path.txt))"
-# TODO
-export EXTERN_EXT_IN="pl_src"
+export EXTERN_EXT_IN="pl"
 
 ${BACKEND_PATH}/bin/driver.sh ${@}
 if [ ${?} -ne 0 ]; then exit 1; fi
