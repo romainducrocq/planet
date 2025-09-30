27198e0bb44ff06d8f1ff9a24164fdd7340e53e1
romainducrocq <romain.g.ducrocq@gmail.com>
2025-09-30 19:42:03 +0300

    todo add m4

--- bin/configure.sh
+++ bin/configure.sh
@@ -3,6 +3,7 @@
 BACKEND_PATH="$(readlink -f $(cat backend_path.txt))"
 export EXTERN_PACKAGE_NAME="planet"
 
+# TODO install m4 as preprocessor
 cd ${BACKEND_PATH}/bin/
 ./configure.sh
 if [ ${?} -ne 0 ]; then exit 1; fi
--- bin/driver.sh
+++ bin/driver.sh
@@ -5,6 +5,7 @@ BACKEND_PATH="$(readlink -f ${PACKAGE_DIR}/$(cat ${PACKAGE_DIR}/backend_path.txt
 # TODO for includes use .etc
 export EXTERN_EXT_IN="plx"
 
+# TODO preprocess with m4 in wheelcc/bin/driver.sh
 ${BACKEND_PATH}/bin/driver.sh ${@}
 if [ ${?} -ne 0 ]; then exit 1; fi
 
