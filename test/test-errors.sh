be6da312d282230c3e20665ee9ef33abb3cd5ff3
romainducrocq <romain.g.ducrocq@gmail.com>
2025-10-02 15:23:25 +0300

    add test errors

--- test/test-errors.sh
+++ test/test-errors.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-echo "TODO"
+BACKEND_PATH="$(readlink -f $(cat ../bin/backend_path.txt))"
+cp print_errors.in ${BACKEND_PATH}/test/tools/
+export EXTERN_EXT_IN="plx"
+export EXTERN_TEST_DIR="${PWD}/tests/compiler"
+export EXTERN_PACKAGE_PATH="$(readlink -f ..)"
+
+cd ${BACKEND_PATH}/test/
+./test-errors.sh ${@}
+if [ ${?} -ne 0 ]; then exit 1; fi
 
 exit 0
