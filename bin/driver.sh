132645af965134b65bc6515b16189b35525b62b2
romainducrocq <romain.g.ducrocq@gmail.com>
2025-10-02 12:48:11 +0300

    add test-all

--- bin/driver.sh
+++ bin/driver.sh
@@ -6,6 +6,7 @@ BACKEND_PATH="$(readlink -f ${PACKAGE_DIR}/$(cat ${PACKAGE_DIR}/backend_path.txt
 export EXTERN_EXT_IN="plx"
 
 # TODO preprocess with m4 in wheelcc/bin/driver.sh
+# TODO link with build/build_lib in wheelcc/bin/driver.sh
 ${BACKEND_PATH}/bin/driver.sh ${@}
 if [ ${?} -ne 0 ]; then exit 1; fi
 
