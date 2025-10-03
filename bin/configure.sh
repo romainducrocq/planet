40a5a2fce983ab12e911de6ba20c16fed5b1bf5b
romainducrocq <romain.g.ducrocq@gmail.com>
2025-10-03 08:37:48 +0300

    config_backend

--- bin/configure.sh
+++ bin/configure.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
 
-BACKEND_PATH="$(readlink -f $(cat backend_path.txt))"
-FILENAME_EXT="plx"
-PACKAGE_NAME="planet"
+BACKEND_PATH="$(readlink -f ../../wheelcc)"
 PACKAGE_PATH="$(readlink -f ..)"
+PACKAGE_NAME="planet"
+FILENAME_EXT="plx"
 
-find . -type l -delete
-if [ ${?} -ne 0 ]; then exit 1; fi
-ln -s ${BACKEND_PATH}/bin/driver.sh ./driver.sh
-if [ ${?} -ne 0 ]; then exit 1; fi
-ln -s ${BACKEND_PATH}/bin/make.sh ./make.sh
-if [ ${?} -ne 0 ]; then exit 1; fi
-ln -s ${BACKEND_PATH}/bin/install.sh ./install.sh
-if [ ${?} -ne 0 ]; then exit 1; fi
+function config_backend () {
+    # bin
+    find . -type l -delete
+    if [ ${?} -ne 0 ]; then return 1; fi
+    ln -s ${BACKEND_PATH}/bin/driver.sh ./driver.sh
+    if [ ${?} -ne 0 ]; then return 1; fi
+    ln -s ${BACKEND_PATH}/bin/make.sh ./make.sh
+    if [ ${?} -ne 0 ]; then return 1; fi
+    ln -s ${BACKEND_PATH}/bin/install.sh ./install.sh
+    if [ ${?} -ne 0 ]; then return 1; fi
 
-cp -v ../frontend/parser/* ${BACKEND_PATH}/src/frontend/parser/
-if [ ${?} -ne 0 ]; then exit 1; fi
-# cp -v ../frontend/intermediate/* ${BACKEND_PATH}/src/frontend/intermediate/
-# if [ ${?} -ne 0 ]; then exit 1; fi
+    # test
+    find ../test/ -type l -delete
+    if [ ${?} -ne 0 ]; then return 1; fi
 
-echo -n "${FILENAME_EXT}" > ${BACKEND_PATH}/bin/filename_ext.txt
-if [ ${?} -ne 0 ]; then
-    echo -e "\033[0;31merror:\033[0m configuration failed" 1>&2
-    exit 1
-fi
-echo -n "${PACKAGE_PATH}" > ${BACKEND_PATH}/bin/package_path.txt
+    # frontend
+    cp ../frontend/parser/* ${BACKEND_PATH}/src/frontend/parser/
+    if [ ${?} -ne 0 ]; then return 1; fi
+    # cp ../frontend/intermediate/* ${BACKEND_PATH}/src/frontend/intermediate/
+    # if [ ${?} -ne 0 ]; then return 1; fi
+
+    # config
+    echo -n "${FILENAME_EXT}" > ${BACKEND_PATH}/bin/filename_ext.txt
+    if [ ${?} -ne 0 ]; then return 1; fi
+    echo -n "${PACKAGE_PATH}" > ${BACKEND_PATH}/bin/package_path.txt
+    if [ ${?} -ne 0 ]; then return 1; fi
+
+    return 0
+}
+
+config_backend
 if [ ${?} -ne 0 ]; then
     echo -e "\033[0;31merror:\033[0m configuration failed" 1>&2
     exit 1
