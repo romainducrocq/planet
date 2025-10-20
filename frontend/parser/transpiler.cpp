3779f486c0210ff2796154a8043cfc513fdc9f1a
romainducrocq <romain.g.ducrocq@gmail.com>
2025-10-20 17:17:45 +0300

    add transpiler

--- frontend/parser/transpiler.cpp
+++ frontend/parser/transpiler.cpp
@@ -3,6 +3,13 @@
 
 cc::Transpiler transpiler;
 
-void cc::Transpiler::Hello() {
-    std::cout << "Hello From Transpiler" << std::endl;
+void cc::Transpiler::add_buffer_line(size_t at, const std::string& line) {
+    if (buffer_lines.find(at) == buffer_lines.end()) {
+        buffer_lines[at] = "";
+    }
+    buffer_lines[at] += line;
+}
+
+void cc::Transpiler::append_text(const std::string& text) {
+
 }
--- frontend/parser/transpiler.h
+++ frontend/parser/transpiler.h
@@ -1,15 +1,23 @@
 #ifndef _TRANSPILER_H_
 #define _TRANSPILER_H_
 
+#include <string>
+#include <unordered_map>
+#include <vector>
+
 namespace cc {
 
 class Transpiler {
+    private:
+        std::unordered_map<size_t, std::string> buffer_lines;
+        std::vector<std::string> lines;
 
     public:
         Transpiler() = default;
         ~Transpiler() = default;
 
-        void Hello();
+        void add_buffer_line(size_t at, const std::string& line);
+        void append_text(const std::string& text);
 };
 
 }
