e57d77a028ee6bee77387498794cab9276e39171
romainducrocq <romain.g.ducrocq@gmail.com>
2025-10-23 17:13:25 +0300

    add test-compiler

--- test/test-compiler.sh
+++ test/test-compiler.sh
@@ -5,7 +5,7 @@ if [[ "${RESULT}" == *"FAIL:"* ]]; then
     exit 1
 fi
 
-export TOTAL_CC=$(("$(echo "${RESULT}" | cut -d":" -f2- | tr -d ' ' | cut -d"/" -f1)"))
+export TOTAL_CC=$(($(echo "${RESULT}" | cut -d":" -f2- | tr -d ' ' | cut -d"/" -f1)))
 ./test-compiler-plx.sh ${@}
 if [ ${?} -ne 0 ]; then exit 1; fi
 
