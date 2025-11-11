9adfc1cd7a703b610a6831d4f0f22ca54d96621f
romainducrocq <romain.g.ducrocq@gmail.com>
2025-11-11 14:36:58 +0200

    parse double

--- frontend/parser/lexer.cpp
+++ frontend/parser/lexer.cpp
@@ -737,8 +737,7 @@ static error_t tokenize_file(Ctx ctx) {
                 case TOK_long_const:
                 case TOK_uint_const:
                 case TOK_ulong_const:
-                case TOK_dbl_const: 
-                {
+                case TOK_dbl_const: {
                     match = get_match(ctx, ctx->match_at, ctx->match_size);
                     match_tok = make_string_identifier(ctx->identifiers, &match);
                     goto Lpass;
