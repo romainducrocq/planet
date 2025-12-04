df35dd6fcdb0fbd74c429c02939bcef3854c9362
romainducrocq <romain.g.ducrocq@gmail.com>
2025-12-04 13:00:17 +0200

    add print_results tool

---
+++ test/tools/print_results.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+RESULTS_DIR="$(dirname "${0}")/../test_results"
+if [ ! -z "${1}" ]; then
+    RESULTS_DIR="${RESULTS_DIR}/${1}"
+else
+    RESULTS_DIR="${RESULTS_DIR}/1"
+fi
+
+cd ${RESULTS_DIR}
+for FILE in $(ls -lt | grep "\.out\.1" | rev | cut -d' ' -f1 | rev | sed '1!G;h;$!d'); do
+    echo "./${FILE}"
+    cat "${FILE}" | tail -1
+done
+
+exit 0
