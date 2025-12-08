2a906dcb3eeb2ec2766687dbc1886abaaa252aa0
romainducrocq <romain.g.ducrocq@gmail.com>
2025-12-08 14:55:48 +0200

    add libc bindings

--- libstd/wrap_stdlib.c
+++ libstd/wrap_stdlib.c
@@ -3,19 +3,3 @@
 int get_EXIT_FAILURE(void) { return EXIT_FAILURE; }
 int get_EXIT_SUCCESS(void) { return EXIT_SUCCESS; }
 int get_RAND_MAX(void) { return RAND_MAX; }
-
-static int bsearch_fptr(const void* a, const void* b) {
-    extern int bsearch_compare(const void* a, const void* b);
-    return bsearch_compare(a, b);
-}
-
-static int qsort_fptr(const void* a, const void* b) {
-    extern int qsort_compare(const void* a, const void* b);
-    return qsort_compare(a, b);
-}
-
-void* bsearch_f(const void *key, const void *base, size_t nmemb, size_t size) {
-    return bsearch(key, base, nmemb, size, bsearch_fptr);
-}
-
-void qsort_f(void *base, size_t nmemb, size_t size) { qsort(base, nmemb, size, qsort_fptr); }
