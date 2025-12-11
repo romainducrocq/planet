41ae9059be6fe336207fdb1e9177ac41899c5e90
romainducrocq <romain.g.ducrocq@gmail.com>
2025-12-11 09:57:53 +0200

    build libc runtime

--- bin/build_libc.sh
+++ bin/build_libc.sh
@@ -10,24 +10,41 @@ if [[ "$(uname -s)" = "Darwin"* ]]; then
 fi
 
 CC_FLAGS="-O3 -Wall -Wextra -Wpedantic -pedantic-errors"
-LIBC_NAME="${PACKAGE_LIBC}/libplx.so"
+LIBC_NAME="${PACKAGE_LIBC}/libplanet.so"
 
-OBJECT_FILES=""
-function build_obj () {
-    FILE="${PACKAGE_LIBC}/${1}"
-    OBJECT="${FILE%.*}.o"
-    OBJECT_FILES="${OBJECT_FILES} ${OBJECT}"
-    echo "${FILE} -> ${OBJECT}"
-    ${CC} ${FILE} ${CC_FLAGS} -c -fPIC -o ${OBJECT}
+function build_lib () {
+    LIB_NAME="${PACKAGE_LIBC}/${1}"
+    FILES_SIZE=${2}
+    FILES="${@:3:${FILES_SIZE}}"
+    FLAGS="${@:$((3+${FILES_SIZE}))}"
+    BUILD_FLAGS="${CC_FLAGS} ${FLAGS}"
+    OBJECT_FILES=""
+    for FILE in ${FILES}; do
+        FILE="${PACKAGE_LIBC}/${FILE}"
+        OBJECT="${FILE%.*}.o"
+        OBJECT_FILES="${OBJECT_FILES} ${OBJECT}"
+        if [ "${LIB_NAME}" = "${LIBC_NAME}" ]; then
+            echo "${FILE} -> ${OBJECT}"
+        fi
+        ${CC} ${FILE} ${BUILD_FLAGS} -c -fPIC -o ${OBJECT}
+        if [ ${?} -ne 0 ]; then exit 1; fi
+    done
+    if [ ! "${LIB_NAME}" = "${LIBC_NAME}" ]; then
+        echo "${FILE} ${FLAGS} -> ${LIB_NAME}"
+    fi
+    ${CC} ${OBJECT_FILES} ${BUILD_FLAGS} -shared -o ${LIB_NAME}
     if [ ${?} -ne 0 ]; then exit 1; fi
 }
 
 echo "-- Build libc ..."
-build_obj "wrap_defs.c"
-build_obj "wrap_vargs.c"
 
-${CC} ${OBJECT_FILES} ${CC_FLAGS} -shared -o ${LIBC_NAME}
-if [ ${?} -ne 0 ]; then exit 1; fi
+build_lib "$(basename "${LIBC_NAME}")" 2 "wrap_defs.c" "wrap_vargs.c"
+build_lib "libmath2.so" 1 "wrap_math.c" "-lm"
+build_lib "libsignal.so" 1 "wrap_fptrs.c" "-DBUILD_SIGNAL"
+build_lib "libat_quick_exit.so" 1 "wrap_fptrs.c" "-DBUILD_AT_QUICK_EXIT"
+build_lib "libatexit.so" 1 "wrap_fptrs.c" "-DBUILD_ATEXIT"
+build_lib "libbsearch.so" 1 "wrap_fptrs.c" "-DBUILD_BSEARCH"
+build_lib "libqsort.so" 1 "wrap_fptrs.c" "-DBUILD_QSORT"
 
 echo "-- Created lib ${LIBC_NAME}"
 
