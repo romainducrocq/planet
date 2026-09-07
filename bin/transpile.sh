6a772117b7e9bd9074db43be331a51f05256c164
romainducrocq <romain.g.ducrocq@gmail.com>
2026-09-07 15:49:19 +0300

    format block separations, temp reserved keywords

--- bin/transpile.sh
+++ bin/transpile.sh
@@ -45,6 +45,14 @@ for CC_DIR in $(find ${CC_PATH} -mindepth 1 -maxdepth 1 -type d); do
         PLX_FILE="${PLX_DIR}/$(basename ${CC_FILE%.*}).plx"
         gcc -E -P ${CC_FILE} -o ${PLX_FILE}
         if [ ${?} -ne 0 ]; then exit 1; fi
+
+        # TODO rm after rename kws
+        sed -i 's/\<get_type_fmt\>/get_type_kw_fmt/' ${PLX_FILE}
+        sed -i 's/\<type\>/type_kw/' ${PLX_FILE}
+        sed -i 's/\<match\>/match_kw/' ${PLX_FILE}
+        sed -i 's/\<string\>/string_kw/' ${PLX_FILE}
+        #
+
         format ${PLX_FILE}
         if [ ${?} -ne 0 ]; then exit 1; fi
         transpiler-planet -v ${PLX_FILE}
